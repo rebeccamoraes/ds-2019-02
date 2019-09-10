dde37fda963294fc140b6452dd6696ac3c80e503
kyriosdata <fabio@inf.ufg.br>
2019-09-10 10:37:06 -0300

    Enum mais adequado que strings

---
+++ builder/src/main/java/com/github/rebeccamoraes/exemplo/domain/Pao.java
@@ -0,0 +1,13 @@
+package com.github.rebeccamoraes.exemplo.domain;
+
+/**
+ * TODO definir corretamente os tipos de pão.
+ */
+public enum Pao {
+
+    FRANCES,
+
+    PORTUGUES,
+
+    BRASILEIRO;
+}
