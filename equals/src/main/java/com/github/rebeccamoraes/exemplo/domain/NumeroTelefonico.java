74299b915962e72a65c861c4da9d97d6b3bb0798
Rebecca Moraes <rebecca@ufg.br>
2019-09-25 21:33:33 -0300

    Criando classe com sobrescrita do método equals.

---
+++ equals/src/main/java/com/github/rebeccamoraes/exemplo/domain/NumeroTelefonico.java
@@ -0,0 +1,37 @@
+package com.github.rebeccamoraes.exemplo.domain;
+
+public class NumeroTelefonico {
+
+    private String codigoPais;
+    private String codigoEstado;
+    private String numero;
+
+    public NumeroTelefonico(String codigoPais, String codigoEstado, 
+        String numero) {
+
+    }
+
+    @Override
+    public boolean equals(final Object outroNumero) {
+        if(outroNumero == null) {
+            return false;
+        }
+        
+        if(this == outroNumero) {
+            return true;
+        }
+        
+        if(outroNumero instanceof NumeroTelefonico) {
+            NumeroTelefonico outroNumeroTel = (NumeroTelefonico) outroNumero;
+            if(outroNumeroTel.codigoPais == this.codigoPais
+                && outroNumeroTel.codigoEstado == this.codigoEstado
+                && outroNumeroTel.numero == this.numero) {
+                    return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+}
